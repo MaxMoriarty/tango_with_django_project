938ec9974e057457d5b9049767b4f65b5c8c6032
Max Moriarty <2642123M@student.gla.ac.uk>
2023-02-06 13:47:42 +0000

    chapter 8 after exercises

--- rango/views.py
+++ rango/views.py
@@ -47,7 +47,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form':form})
@@ -59,7 +59,7 @@ def add_page(request, category_name_slug):
         category=None
 
     if category is None:
-        return redirect('/rango')
+        return redirect(reverse('rangi:index'))
 
     form = PageForm()
 
